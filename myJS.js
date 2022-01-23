8452247386122484cf2f1538bdbf4746958ce9a3
Churchill Capagngan <97002777+CBCapagngan@users.noreply.github.com>
2022-01-23 17:41:43 +0800

    Update myJS.js

--- myJS.js
+++ myJS.js
@@ -2,7 +2,7 @@ let def = "when I was a kid, I really love playing video games.<br>";
 let def1 = "after I learn that video games are created by Game Devs,<br>";
 let def2 = "I decided to take that course.";
 
-function change(sw){	
+function change(){	
 	document.getElementById("Introduction").innerHTML = 
 	def + def1 + def2;
 }
